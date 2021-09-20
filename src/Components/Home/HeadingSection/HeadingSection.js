5e5dd944341bef7ca0d0bcaf11ecddb8698a9b05
ishan <ishan.hcl.tab1@gmail.com>
2021-09-20 17:10:00 +0530

    add responsive design for mobile and desktop

--- src/Components/Home/HeadingSection/HeadingSection.js
+++ src/Components/Home/HeadingSection/HeadingSection.js
@@ -1,11 +1,18 @@
 import React from "react";
 import classes from "./HeadingSection.module.css";
 import headingBackground from "../../../assets/images/headingBackground.jpg";
+import headingBackground90 from "../../../assets/images/headingBackground90.jpg";
 
 function HeadingSection() {
   return (
     <div className={classes.HeadingSection}>
-      <img src={headingBackground} alt="" />
+      <img src={headingBackground} className={classes.ImageForDesktop} alt="" />
+      <img
+        src={headingBackground90}
+        className={classes.ImageForMobile}
+        alt=""
+      />
+
       <div className={classes.Details}>
         <h1 className={classes.Heading}>Sample Heading</h1>
         <div className={classes.HeadingDescription}>
